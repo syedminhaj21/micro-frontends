cb45a432d7bac5fbd9c366feced46d1da2fd7a10
syedminhaj21 <syedminhaj78621@gmail.com>
2022-08-13 21:02:08 +0530

    updated test

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -6,7 +6,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Hi There!</h1>
+      <h1>Container App</h1>
       <hr />
       <MarketingApp />
     </div>
